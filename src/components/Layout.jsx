7dcee2e20cd487f0c8874da381de0c8f8f6e729e
Gram Liu <gram.liu226@gmail.com>
2021-08-28 12:49:46 -0400

    Removed loader from base layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,39 +1,24 @@
 import { makeStyles } from "@material-ui/core"
-import React, { useState } from "react"
+import React from "react"
 import { Helmet } from "react-helmet"
-import Loader from "./Loader"
 
 const useStyles = makeStyles((theme) => ({
   container: {
     height: "100vh",
     width: "100vw",
   },
-  loader: {
-    height: "100vh",
-    width: "100vw",
-    position: "absolute",
-  },
 }))
 
 const Layout = ({ children }) => {
   const classes = useStyles()
-  const [hidden, setHidden] = useState(true)
 
-  const finishLoading = () => {
-    setHidden(false)
-  }
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Gram Liu</title>
       </Helmet>
-      <Loader finishLoading={finishLoading} className={classes.loader} />
-      <div
-        id="children"
-        className={classes.container}
-        hidden={hidden}
-      >
+      <div id="children" className={classes.container}>
         {children}
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,38 @@
-import React from "react"
+import { makeStyles } from "@material-ui/core"
+import React, { useState } from "react"
 import "src/styles/global.css"
 import About from "../components/About"
 import Activities from "../components/Activities"
 import Home from "../components/Home"
+import Loader from "../components/Loader"
 import Navbar from "../components/Navbar"
 import Portfolio from "../components/Portfolio"
 
+const useStyles = makeStyles((theme) => ({
+  loader: {
+    height: "100vh",
+    width: "100vw",
+    position: "absolute",
+  },
+}))
+
 const IndexPage = () => {
+  const classes = useStyles()
+  const [hidden, setHidden] = useState(true)
+
+  const finishLoading = () => {
+    setHidden(false)
+  }
   return (
     <>
-      <Navbar />
-      <Home />
-      <About />
-      <Activities />
-      <Portfolio />
+      <Loader finishLoading={finishLoading} className={classes.loader} />
+      <div hidden={hidden}>
+        <Navbar />
+        <Home />
+        <About />
+        <Activities />
+        <Portfolio />
+      </div>
     </>
   )
 }
