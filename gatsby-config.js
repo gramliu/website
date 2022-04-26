ace36271c5059492297854a742fa090ecfc56f69
Gram Liu <gram.liu226@gmail.com>
2022-04-26 14:30:56 -0400

    Added SEO description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://gramliu.com/",
     title: "Gram Liu",
+    description: "Hi! I'm Gram and I build things, from web apps to full stack to IoT. I'm a rising junior at Carnegie Mellon University pursuing a major in Electrical and Computer Engineering. I am a big fan of technology and how it changes the way we think about and address problems from education to health care, consistently pushing the boundaries of what we think is possible."
   },
   plugins: [
     {
