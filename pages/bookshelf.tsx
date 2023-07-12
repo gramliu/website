33233bc1349c8e8f764af46473a853e4ca080e18
Gram Liu <gram.liu226@gmail.com>
2023-07-12 11:30:40 -0700

    Create bookshelf page

---
+++ pages/bookshelf.tsx
@@ -0,0 +1,10 @@
+import Layout from "../src/components/Layout";
+import NavPill from "../src/components/NavPill";
+
+export default function Bookshelf() {
+  return (
+    <Layout>
+      <NavPill />
+    </Layout>
+  );
+}
