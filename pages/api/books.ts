d90f94f2feed618ba0e905b2097f10968223c41b
Gram Liu <gram.liu226@gmail.com>
2023-08-13 23:55:43 -0700

    Fix dev caching condition

--- pages/api/books.ts
+++ pages/api/books.ts
@@ -24,59 +24,60 @@ export default async function handler(
 ) {
   if (env.NODE_ENV === "development") {
     res.json(booksCached);
-  } else {
-    const html = await axios.get(url);
-    const $ = cheerio.load(html.data);
-
-    const booksBody = $("#booksBody tr");
-
-    // Construct books array
-    const books = booksBody
-      .map((i, tr) => {
-        const imageUrl =
-          $(tr)
-            .find(".cover > .value a > img")
-            .attr("src")
-            // Remove suffixes of the form ._SX75_, ._SY50_, or ._SX50_SY75_
-            ?.replace(/\.((_SX[0-9]{2})?(_SY[0-9]{2})|(_SX[0-9]{2}))_/, "") ??
-          "";
-
-        return {
-          title: $(tr).find(".title > .value > a").attr("title")?.trim() ?? "",
-          author: $(tr).find(".author > .value > a").text().trim(),
-          url: `https://www.goodreads.com${
-            $(tr).find(".title > .value > a").attr("href") ?? ""
-          }`,
-          imageUrl,
-        };
-      })
-      .toArray();
-
-    // Download images into base64
-    const images = await Promise.all(
-      books.map((book) =>
-        axios.get(book.imageUrl, { responseType: "arraybuffer" })
-      )
-    );
-    const imageData = images.map(
-      (image) =>
-        `data:image/jpeg;base64,${Buffer.from(image.data, "binary").toString(
-          "base64"
-        )}`
-    );
-
-    // Extract colors from images
-    const rgb = await Promise.all(imageData.map((image) => getColor(image)));
-    const fgColors = rgb.map(getForegroundColor);
-    const bgColors = rgb.map((rgb) => `#${rgbHex(rgb[0], rgb[1], rgb[2])}`);
-    const booksWithColors: Book[] = books.map((book, i) => ({
-      ...book,
-      fgColor: fgColors[i],
-      bgColor: bgColors[i],
-    }));
-
-    res.json(booksWithColors);
+    return;
   }
+
+  const html = await axios.get(url);
+  const $ = cheerio.load(html.data);
+
+  const booksBody = $("#booksBody tr");
+
+  // Construct books array
+  const books = booksBody
+    .map((i, tr) => {
+      const imageUrl =
+        $(tr)
+          .find(".cover > .value a > img")
+          .attr("src")
+          // Remove suffixes of the form ._SX75_, ._SY50_, or ._SX50_SY75_
+          ?.replace(/\.((_SX[0-9]{2})?(_SY[0-9]{2})|(_SX[0-9]{2}))_/, "") ?? "";
+      console.log(imageUrl);
+
+      return {
+        title: $(tr).find(".title > .value > a").attr("title")?.trim() ?? "",
+        author: $(tr).find(".author > .value > a").text().trim(),
+        url: `https://www.goodreads.com${
+          $(tr).find(".title > .value > a").attr("href") ?? ""
+        }`,
+        imageUrl,
+      };
+    })
+    .toArray();
+
+  // Download images into base64
+  const images = await Promise.all(
+    books.map((book) =>
+      axios.get(book.imageUrl, { responseType: "arraybuffer" })
+    )
+  );
+  const imageData = images.map(
+    (image) =>
+      `data:image/jpeg;base64,${Buffer.from(image.data, "binary").toString(
+        "base64"
+      )}`
+  );
+
+  // Extract colors from images
+  const rgb = await Promise.all(imageData.map((image) => getColor(image)));
+  const fgColors = rgb.map(getForegroundColor);
+  const bgColors = rgb.map((rgb) => `#${rgbHex(rgb[0], rgb[1], rgb[2])}`);
+  const booksWithColors: Book[] = books.map((book, i) => ({
+    ...book,
+    fgColor: fgColors[i],
+    bgColor: bgColors[i],
+  }));
+
+  res.json(booksWithColors);
 }
 
 /**
