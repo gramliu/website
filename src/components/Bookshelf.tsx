3763b1348e5a4698ac1d3cc81a661ba620980254
Gram Liu <gram.liu226@gmail.com>
2023-09-04 23:36:04 -0700

    Make overflow-x auto

--- src/components/Bookshelf.tsx
+++ src/components/Bookshelf.tsx
@@ -12,7 +12,7 @@ function InteractiveBookshelf({ books }: { books: Book[] }) {
   return (
     <div
       role="list"
-      className="md:flex flex-row flex-start gap-4 lg:w-8/12 mx-auto hidden overflow-x-scroll"
+      className="md:flex flex-row flex-start gap-4 lg:w-8/12 mx-auto hidden overflow-x-auto"
     >
       {books.map((book, index) => (
         <button
