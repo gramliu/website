83155478cb94e4ae091cee97136c29dd459be55b
Gram Liu <gram.liu226@gmail.com>
2023-11-20 22:06:05 -0800

    Add pagination to research papers

--- src/components/Papers.tsx
+++ src/components/Papers.tsx
@@ -1,7 +1,8 @@
-import { ResearchPaper } from "../server/getPapers";
+import { useMemo, useState } from "react";
 import LinkIcon from "../icons/link";
+import { ResearchPaper } from "../server/getPapers";
 
-function ResearchPaperComponent({ paper }: { paper: ResearchPaper }) {
+function PaperEntry({ paper }: { paper: ResearchPaper }) {
   return (
     <div className="flex flex-col">
       <a
@@ -22,22 +23,68 @@ function ResearchPaperComponent({ paper }: { paper: ResearchPaper }) {
   );
 }
 
-export default function Papers({ papers }: { papers: ResearchPaper[] }) {
+const PAGE_SIZE = 10;
+
+/**
+ * Paginates an array into an array of arrays of length `pageSize`.
+ */
+function paginate<T>(array: T[], pageSize: number): T[][] {
+  const pages = [];
+  for (let i = 0; i < array.length; i += pageSize) {
+    pages.push(array.slice(i, i + pageSize));
+  }
+  return pages;
+}
+
+export default function Papers({
+  papers,
+  className,
+}: {
+  papers: ResearchPaper[];
+  className: string;
+}) {
+  // Group papers into pages
+  const pages = useMemo(() => paginate(papers, PAGE_SIZE), [papers]);
+  const [page, setPage] = useState(0);
+
   return (
-    <>
+    <div className={className}>
       <div
         id="papers"
         className="flex items-center justify-center text-center w-full font-bold text-3xl mb-8 mx-auto"
       >
         Recently Read Papers
       </div>
-      <div className="flex flex-row items-center justify-center p-4">
-        <div className="flex flex-col gap-4 items-start">
-          {papers.map((paper) => (
-            <ResearchPaperComponent paper={paper} key={paper.title} />
+      <div className="flex flex-col items-center justify-center p-4">
+        <div className="flex flex-col gap-4 w-full md:w-8/12 h-128 items-start">
+          {pages[page].map((paper) => (
+            <PaperEntry paper={paper} key={paper.title} />
           ))}
         </div>
+        {/* Pagination controls */}
+        <div className="flex flex-row gap-2 ml-4 mt-10">
+          {/* Previous button */}
+          <button
+            className="flex items-center justify-center text-2xl font-black text-text-primary h-8"
+            onClick={() => setPage(Math.max(page - 1, 0))}
+            disabled={page === 0}
+          >
+            &lt;
+          </button>
+          {/* Page number */}
+          <div className="flex items-center tracking-widest justify-center text-2xl font-light text-white w-24 h-8">
+            {page + 1}/{pages.length}
+          </div>
+          {/* Next button */}
+          <button
+            className="flex items-center justify-center text-2xl font-black text-white h-8"
+            onClick={() => setPage(Math.min(page + 1, pages.length - 1))}
+            disabled={page === pages.length - 1}
+          >
+            &gt;
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
