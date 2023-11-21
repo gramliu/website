e8e89d1bf9b1f5451dd8ef52e24237adc5f88c8d
Gram Liu <gram.liu226@gmail.com>
2023-11-20 22:23:05 -0800

    Move pagination up

--- src/components/Papers.tsx
+++ src/components/Papers.tsx
@@ -36,6 +36,41 @@ function paginate<T>(array: T[], pageSize: number): T[][] {
   return pages;
 }
 
+function PaginationControls({
+  page,
+  pages,
+  setPage,
+}: {
+  page: number;
+  pages: ResearchPaper[][];
+  setPage: (page: number) => void;
+}) {
+  return (
+    <div className="flex flex-row gap-2 ml-4 mb-10">
+      {/* Previous button */}
+      <button
+        className="flex items-center justify-center text-2xl font-black text-text-primary h-8"
+        onClick={() => setPage(Math.max(page - 1, 0))}
+        disabled={page === 0}
+      >
+        &lt;
+      </button>
+      {/* Page number */}
+      <div className="flex items-center tracking-widest justify-center text-2xl font-light text-white w-24 h-8">
+        {page + 1}/{pages.length}
+      </div>
+      {/* Next button */}
+      <button
+        className="flex items-center justify-center text-2xl font-black text-white h-8"
+        onClick={() => setPage(Math.min(page + 1, pages.length - 1))}
+        disabled={page === pages.length - 1}
+      >
+        &gt;
+      </button>
+    </div>
+  );
+}
+
 export default function Papers({
   papers,
   className,
@@ -56,33 +91,14 @@ export default function Papers({
         Recently Read Papers
       </div>
       <div className="flex flex-col items-center justify-center p-4">
+        <PaginationControls
+          page={page}
+          pages={pages}
+          setPage={setPage} />
         <div className="flex flex-col gap-4 w-full md:w-8/12 h-128 items-start">
           {pages[page].map((paper) => (
             <PaperEntry paper={paper} key={paper.title} />
           ))}
-        </div>
-        {/* Pagination controls */}
-        <div className="flex flex-row gap-2 ml-4 mt-10">
-          {/* Previous button */}
-          <button
-            className="flex items-center justify-center text-2xl font-black text-text-primary h-8"
-            onClick={() => setPage(Math.max(page - 1, 0))}
-            disabled={page === 0}
-          >
-            &lt;
-          </button>
-          {/* Page number */}
-          <div className="flex items-center tracking-widest justify-center text-2xl font-light text-white w-24 h-8">
-            {page + 1}/{pages.length}
-          </div>
-          {/* Next button */}
-          <button
-            className="flex items-center justify-center text-2xl font-black text-white h-8"
-            onClick={() => setPage(Math.min(page + 1, pages.length - 1))}
-            disabled={page === pages.length - 1}
-          >
-            &gt;
-          </button>
         </div>
       </div>
     </div>
