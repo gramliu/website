d34e083d7e7840011654e5448e86a0cbf1e60031
Gram Liu <gram.liu226@gmail.com>
2024-02-02 09:29:15 -0800

    Create blogs config

---
+++ src/config/blogs.tsx
@@ -0,0 +1,17 @@
+interface Blog {
+  title: string;
+  url: string;
+}
+
+const blogs: Blog[] = [
+  {
+    title: "Do things that don't scale",
+    url: "https://www.ycombinator.com/library/96-do-things-that-don-t-scale"
+  },
+  {
+    title: "Idea Generation"
+    url: "https://blog.samaltman.com/idea-generation"
+  }
+];
+
+export default blogs;
