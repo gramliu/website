4602417ca0137d815fc7ebe2238e38368bbc103d
Gram Liu <gram.liu226@gmail.com>
2024-02-02 20:16:08 -0800

    Add author to blog config

--- src/config/blogs.tsx
+++ src/config/blogs.tsx
@@ -1,16 +1,19 @@
 interface Blog {
   title: string;
   url: string;
+  author: string;
 }
 
 const blogs: Blog[] = [
   {
     title: "Do things that don't scale",
-    url: "https://www.ycombinator.com/library/96-do-things-that-don-t-scale"
+    url: "https://www.ycombinator.com/library/96-do-things-that-don-t-scale",
+    author: "Paul Graham"
   },
   {
-    title: "Idea Generation"
-    url: "https://blog.samaltman.com/idea-generation"
+    title: "Idea Generation",
+    url: "https://blog.samaltman.com/idea-generation",
+    author: "Sam Altman"
   }
 ];
 
