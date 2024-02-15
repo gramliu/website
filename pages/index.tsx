4d98e33c9ed6d927807c3b36a3d9499290ce3d0a
Gram Liu <gram.liu226@gmail.com>
2024-02-14 18:17:34 -0800

    Remove tracing beam

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { MDXProvider, MergeComponents } from "@mdx-js/react/lib";
+import { MDXComponents } from "mdx/types";
 import Link from "next/link";
+import { ReactNode } from "react";
 import AboutSection from "../src/components/AboutSection";
 import ActivitiesList from "../src/components/ActivitiesList";
 import Footer from "../src/components/Footer";
@@ -7,10 +10,6 @@ import Layout from "../src/components/Layout";
 import NavPill from "../src/components/NavPill";
 import Portfolio from "../src/components/Portfolio";
 import ProjectArchive from "../src/components/ProjectArchive";
-import { ReactNode } from "react";
-import { MDXComponents } from "mdx/types";
-import { MDXProvider, MergeComponents } from "@mdx-js/react/lib";
-import { TracingBeam } from "../src/components/ui/tracing-beam";
 
 const mdxComponents = {
   a: ({ children, href }: { children: ReactNode; href: string }) => (
@@ -29,15 +28,13 @@ export default function HomePage() {
   return (
     <MDXProvider components={mdxComponents}>
       <Layout>
-        <TracingBeam className="w-screen">
-          <NavPill />
-          <Hero />
-          <AboutSection />
-          <ActivitiesList />
-          <Portfolio />
-          <ProjectArchive />
-          <Footer />
-        </TracingBeam>
+        <NavPill />
+        <Hero />
+        <AboutSection />
+        <ActivitiesList />
+        <Portfolio />
+        <ProjectArchive />
+        <Footer />
       </Layout>
     </MDXProvider>
   );
