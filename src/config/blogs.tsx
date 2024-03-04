ba01b6918013ce0e0d3ab10fde1e393057a74065
Gram Liu <gram.liu226@gmail.com>
2024-03-03 22:12:57 -0800

    Update blogs.tsx

--- src/config/blogs.tsx
+++ src/config/blogs.tsx
@@ -19,6 +19,11 @@ const blogs: Blog[] = [
     title: "How to be Successful",
     url: "https://blog.samaltman.com/how-to-be-successful",
     author: "Sam Altman"
+  },
+  {
+    title: "Economic Inequality",
+    url: "https://paulgraham.com/ineq.html",
+    author: "Paul Graham"
   }
 ];
 
