8a870c351e6758d745f3d4a9c00510a26ff06e74
Gram Liu <gram.liu226@gmail.com>
2024-08-06 01:24:05 -0700

    Add initial tap to pay animation

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,17 +1,18 @@
 import { Element } from "react-scroll";
 import Bio from "./bio.mdx";
+import TapToPayAnimation from "./taptopay";
 
 export default function About() {
   return (
     <>
-      <div
-        className="flex flex-col items-center justify-center pt-10 my-0 mx-auto"
-        id="about"
-      >
-        <Element name="about" />
-        <div className="flex items-center font-bold text-3xl">About Me</div>
-        <div className="w-10/12 md:w-8/12 lg:w-5/12 mt-8 text-lg text-left">
-          <Bio />
+      <div className="grid grid-cols-[1fr,3fr] pt-10 my-0 mx-auto" id="about">
+        <TapToPayAnimation />
+        <div className="flex flex-col items-center justify-center">
+          <Element name="about" />
+          <div className="flex items-center font-bold text-3xl">About Me</div>
+          <div className="w-10/12 md:w-8/12 mt-8 text-lg text-left">
+            <Bio />
+          </div>
         </div>
       </div>
     </>
