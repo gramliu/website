7303d9511d2c6d8c5341b19442c5b12b1c2ed083
Gram Liu <gram.liu226@gmail.com>
2024-08-26 17:00:27 -0700

    Show TTP animation on mobile

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -6,16 +6,20 @@ export default function About() {
   return (
     <>
       <div
-        className="md:grid md:grid-cols-[1fr,3fr] pt-10 my-0 mx-auto"
+        className="flex flex-col pt-10 my-0 mx-auto md:grid md:grid-cols-[1fr,3fr]"
         id="about"
       >
-        {/* TODO: Add animation for mobile */}
-        <div className="flex-col items-center justify-center ml-20 hidden md:flex">
-          <a href="https://stripe.com/terminal/tap-to-pay" target="_blank" rel="noreferrer">
+        <div className="flex flex-col items-center justify-center order-2 md:order-1 md:ml-20">
+          <a
+            href="https://stripe.com/terminal/tap-to-pay"
+            target="_blank"
+            rel="noreferrer"
+            className="mt-20 md:mt-0"
+          >
             <TapToPayAnimation />
           </a>
         </div>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center order-1 md:order-2">
           <Element name="about" />
           <div className="flex items-center font-bold text-3xl">About Me</div>
           <div className="w-10/12 md:w-8/12 mt-8 text-lg text-left">
